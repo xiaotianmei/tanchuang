adbbc69212ce1ae4debfe07283e190a720b28295
xiaotianmei <1025747576@qq.com>
2017-11-10 17:14:44 +0800

    弹窗实例

---
+++ js/base.js
@@ -0,0 +1,46 @@
+﻿function login() {
+    var returnUrl = jQuery.getUrlParam("ReturnUrl");
+    if (returnUrl == null || returnUrl == "") {
+        returnUrl = document.URL;
+        if (returnUrl.toLowerCase().indexOf("/user/") > -1 || returnUrl.toLowerCase().indexOf("/member/resetpwd.aspx") > -1) {
+            return location.href = Globals.PortalWebsiteUrl + "/user/login.aspx", !1
+        }
+    }
+
+    return location.href = Globals.PortalWebsiteUrl + "/user/login.aspx?ReturnUrl=" + returnUrl,
+    !1
+}
+
+function register() {
+    var returnUrl = jQuery.getUrlParam("ReturnUrl");
+    if (returnUrl == null || returnUrl == "") {
+        returnUrl = document.URL;
+        if (returnUrl.toLowerCase().indexOf("/user/") > -1 || returnUrl.toLowerCase().indexOf("/member/resetpwd.aspx") > -1 || returnUrl.toLowerCase().indexOf(Globals.PortalWebsiteUrl) > -1) {
+            return location.href = Globals.PortalWebsiteUrl + "/user/register.aspx", !1
+        }
+    }
+
+    return location.href = Globals.PortalWebsiteUrl + "/user/register.aspx?ReturnUrl=" + returnUrl,
+    !1
+}
+
+function logout() {
+    var returnUrl = jQuery.getUrlParam("ReturnUrl");
+    if (returnUrl == null || returnUrl == "") {
+        returnUrl = document.URL;
+        if (returnUrl.toLowerCase().indexOf("/user/") > -1 || returnUrl.toLowerCase().indexOf("/member/resetpwd.aspx") > -1) {
+            return location.href = Globals.PortalWebsiteUrl + "/user/logout.aspx", !1
+        }
+    }
+
+    return location.href = Globals.PortalWebsiteUrl + "/user/logout.aspx?ReturnUrl=" + returnUrl,
+    !1
+}
+
+(function (jQuery) {
+    jQuery.getUrlParam = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    }
+})(jQuery);
---
+++ js/tongjicode.js
@@ -0,0 +1,30 @@
+﻿
+var _mvq = window._mvq || []; 
+window._mvq = _mvq;
+_mvq.push(['$setAccount', 'm-250005-0']);
+_mvq.push(['$setGeneral', '', '', /*用户名*/ '', /*用户id*/ '']);//如果不传用户名、用户id，此句可以删掉
+_mvq.push(['$logConversion']);
+(function() {
+    var mvl = document.createElement('script');
+    mvl.type = 'text/javascript'; mvl.async = true;
+    mvl.src = ('https:' == document.location.protocol ? 'https://static-ssl.mediav.com/mvl.js' : 'http://static.mediav.com/mvl.js');
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(mvl, s); 
+})();
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?d49ed4ba53187102a4f3f986b0217f4a";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
+
+var gdt_tracker = gdt_tracker || [];
+gdt_tracker.push(["set_source_id", "31464"]);
+(function() {
+    var doc=document, h=doc.getElementsByTagName("head")[0], s=doc.createElement("script");
+    s.async=true; s.src="http://qzs.qq.com/qzone/biz/res/gt.js";
+    h && h.insertBefore(s,h.firstChild)
+})();
+
